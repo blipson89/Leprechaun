35bd307f4bc8a54e9bff853f07fc2b4d7fcb3f40
Ben Lipson <blipson89@gmail.com>
2018-05-28 17:32:32 -0400

    #7 Added template's own fields to validation and a bunch of unit tests

---
+++ src/Leprechaun.Tests/Test/SpecimenBuilders/StandardArchitectorValidator/DuplicateTemplateNamesSpec.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoFixture;
+using AutoFixture.Kernel;
+using Leprechaun.Model;
+
+namespace Leprechaun.Tests.Test.SpecimenBuilders.StandardArchitectorValidator
+{
+	public class DuplicateTemplateNamesSpec : ITemplateMetadataSpec
+	{
+		protected List<TemplateCodeGenerationMetadata> Metadatas;
+		private bool _init;
+		public void Init(IFixture fixture)
+		{
+			Metadatas = fixture.CreateMany<TemplateCodeGenerationMetadata>(5).ToList();
+			Metadatas.Add(Metadatas[0]);
+
+			_init = true;
+		}
+
+		public object Create(object request, ISpecimenContext context)
+		{
+			if (!_init) throw new InvalidOperationException("Init not called");
+
+			if (!(request is ParameterInfo pi)) return new NoSpecimen();
+
+			if (pi.Name == "allTemplates")
+				return Metadatas;
+
+			return new NoSpecimen();
+		}
+	}
+}
---
+++ src/Leprechaun.Tests/Test/SpecimenBuilders/XmlNodeBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Xml;
+using AutoFixture.Kernel;
+
+namespace Leprechaun.Tests.Test.SpecimenBuilders
+{
+	public class XmlNodeBuilder : ISpecimenBuilder
+	{
+		public object Create(object request, ISpecimenContext context)
+		{
+			if (!(request is ParameterInfo pi) || pi.ParameterType != typeof(XmlNode)) return new NoSpecimen();
+
+			var xmlDocument = new XmlDocument();
+			xmlDocument.Load(@"Leprechaun.config");
+			return xmlDocument;
+		}
+	}
+}
