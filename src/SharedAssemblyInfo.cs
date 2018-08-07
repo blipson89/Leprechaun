c177622fc1cca04464dba54a2998fa6d8aa5900a
Ben Lipson <blipson89@gmail.com>
2018-08-07 19:59:29 -0400

    Bumped version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1-pre03")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 [assembly: AssemblyProduct("Leprechaun")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
