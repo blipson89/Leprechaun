a8f9350630b08d7172356488121e18340f2ffb33
Ben Lipson <blipson89@gmail.com>
2019-10-17 23:32:26 -0400

    Updated assembly info

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
 [assembly: AssemblyProduct("Leprechaun")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
