740e0fda0fb34f6494362b7afa09cba412076e31
Ben Lipson <blipson89@gmail.com>
2020-05-06 21:39:44 -0400

    Added Leprechaun.Cli

---
+++ src/Leprechaun.Cli/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Leprechaun.Execution;
+
+namespace Leprechaun.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// PARSE ARGS
+			var argsParser = new CommandLineParser.CommandLineParser();
+			var parsedArgs = new ConsoleArgs();
+
+			argsParser.ExtractArgumentAttributes(parsedArgs);
+			argsParser.ParseCommandLine(args);
+
+			if (parsedArgs.Help)
+			{
+				argsParser.ShowUsage();
+				Environment.Exit(-1);
+			}
+
+			var appRunTimer = new Stopwatch();
+			appRunTimer.Start();
+
+			var runner = new Runner();
+			runner.Run(parsedArgs);
+
+			appRunTimer.Stop();
+
+			if (parsedArgs.NoExit)
+			{
+				System.Console.ReadKey();
+			}
+		}
+    }
+}
