f6e9af4f0c894fd33143bdd140c79239dadd186f
Jaya21-git <jayalaxmiarapalli@gmail.com>
2025-01-21 07:40:04 +0530

    Update SerializationModuleConfigurationExtensions.cs

--- src/Leprechaun.InputProviders.Sitecore/Extensions/SerializationModuleConfigurationExtensions.cs
+++ src/Leprechaun.InputProviders.Sitecore/Extensions/SerializationModuleConfigurationExtensions.cs
@@ -3,19 +3,27 @@ using Sitecore.DevEx.Serialization.Client.Configuration;
 
 namespace Leprechaun.InputProviders.Sitecore.Extensions
 {
-	public static class SerializationModuleConfigurationExtensions
+	public static List<string> GetLeprechaunModuleName(this SerializationModuleConfiguration configuration)
 	{
-		public static string GetLeprechaunModuleName(this SerializationModuleConfiguration configuration)
+		if(configuration == null)
+			throw new NullReferenceException("SerializationModuleConfiguration is null");
+	 
+		List<string> configNames = new List<string>(); 
+		if (!configuration.Extensions.ContainsKey("leprechaun"))
+			return new List<string> { configuration.Namespace };
+		var token =	configuration.Extensions["leprechaun"].SelectToken("configuration"); 
+		if (token.Type == JTokenType.Array)
 		{
-			if(configuration == null)
-				throw new NullReferenceException("SerializationModuleConfiguration is null");
-
-			if (!configuration.Extensions.ContainsKey("leprechaun"))
-				return configuration.Namespace;
-
-			var configName = configuration.Extensions["leprechaun"].SelectToken("configuration").Value<string>("@name");
-
-			return string.IsNullOrEmpty(configName) ? configuration.Namespace : configName;
+			foreach (JToken item in token)
+			{
+				configNames.Add(item.Value<string>("@name"));
+			}
 		}
+		else
+		{
+			configNames.Add(token.Value<string>("@name"));
+		}			
+ 
+		return configNames ?? new List<string> { configuration.Namespace };
 	}
 }
