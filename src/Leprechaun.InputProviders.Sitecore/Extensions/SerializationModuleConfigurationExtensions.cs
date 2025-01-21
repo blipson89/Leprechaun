5c588dc432af179e22d7207bd1e628bab0b14316
Jaya21-git <jayalaxmiarapalli@gmail.com>
2025-01-21 11:57:05 +0530

    Update SerializationModuleConfigurationExtensions.cs

--- src/Leprechaun.InputProviders.Sitecore/Extensions/SerializationModuleConfigurationExtensions.cs
+++ src/Leprechaun.InputProviders.Sitecore/Extensions/SerializationModuleConfigurationExtensions.cs
@@ -3,27 +3,30 @@ using Sitecore.DevEx.Serialization.Client.Configuration;
 
 namespace Leprechaun.InputProviders.Sitecore.Extensions
 {
-	public static List<string> GetLeprechaunModuleName(this SerializationModuleConfiguration configuration)
-	{
-		if(configuration == null)
-			throw new NullReferenceException("SerializationModuleConfiguration is null");
-	 
-		List<string> configNames = new List<string>(); 
-		if (!configuration.Extensions.ContainsKey("leprechaun"))
-			return new List<string> { configuration.Namespace };
-		var token =	configuration.Extensions["leprechaun"].SelectToken("configuration"); 
-		if (token.Type == JTokenType.Array)
+	public static class SerializationModuleConfigurationExtensions
+	{	
+		public static List<string> GetLeprechaunModuleName(this SerializationModuleConfiguration configuration)
 		{
-			foreach (JToken item in token)
+			if(configuration == null)
+				throw new NullReferenceException("SerializationModuleConfiguration is null");
+		 
+			List<string> configNames = new List<string>(); 
+			if (!configuration.Extensions.ContainsKey("leprechaun"))
+				return new List<string> { configuration.Namespace };
+			var token =	configuration.Extensions["leprechaun"].SelectToken("configuration"); 
+			if (token.Type == JTokenType.Array)
 			{
-				configNames.Add(item.Value<string>("@name"));
+				foreach (JToken item in token)
+				{
+					configNames.Add(item.Value<string>("@name"));
+				}
 			}
+			else
+			{
+				configNames.Add(token.Value<string>("@name"));
+			}			
+	 
+			return configNames ?? new List<string> { configuration.Namespace };
 		}
-		else
-		{
-			configNames.Add(token.Value<string>("@name"));
-		}			
- 
-		return configNames ?? new List<string> { configuration.Namespace };
 	}
 }
